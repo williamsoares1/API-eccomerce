8c2e943ad08a9b46441504fb73ced6fbdd4fd190
William-Soares <williamsoaresoutlook@gmail.com>
2024-05-14 18:55:40 -0300

    resposta badRequest adicionado

--- trabalho_api/trabalho_api/src/main/java/com/trabalho/api/exception/ControllerHandler.java
+++ trabalho_api/trabalho_api/src/main/java/com/trabalho/api/exception/ControllerHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -23,5 +24,11 @@ public class ControllerHandler extends ResponseEntityExceptionHandler {
                 .map(e -> e.getField() + " - " + e.getDefaultMessage()).collect(Collectors.toList());
 
         return new ResponseEntity<Object>(erros, HttpStatus.NOT_ACCEPTABLE);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+            HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
     }
 }
